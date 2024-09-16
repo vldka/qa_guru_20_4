6725bf0111621977177373f9582e933459beb821
Vlad Kashkarov <kwa-111@yandex.ru>
2024-09-16 15:50:25 +0300

    fix delete sleep

--- src/test/java/JUnit5Test.java
+++ src/test/java/JUnit5Test.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class JUnit5Test {
 
