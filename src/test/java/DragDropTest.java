cdb33ad0e000016bc557d1066c285df7b234d334
Vlad Kashkarov <kwa-111@yandex.ru>
2024-09-16 14:10:53 +0300

    add: class DragDropTest

---
+++ src/test/java/DragDropTest.java
@@ -0,0 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.DragAndDropOptions.to;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragDropTest {
+    @BeforeAll
+    static void openPage() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @AfterAll
+    static void closeWebDriver() {
+        WebDriverRunner.closeWebDriver();
+    }
+
+    @Test
+    void dragDropTest() {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDrop(to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+
+    @Test
+    void actionTest() {
+        open("/drag_and_drop");
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
